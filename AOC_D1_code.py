a3e44db1253c73d5dfaeffd8fbeb6c0848b64ed8
jim-gardiner <jim.gardiner@gmail.com>
2022-12-14 14:40:30 +0000

    day 1 completed

--- AOC_D1_code.py
+++ AOC_D1_code.py
@@ -2,7 +2,9 @@
 
 file1 = open('AOC_2022_day1_input', 'r')
 count = 0
-  
+calorie_counter = 0 
+highest_count = 0
+highest_count_list = []
 while True:
     count += 1
   
@@ -13,6 +15,33 @@ while True:
     # end of file is reached
     if not line:
         break
-    print(line)
+    
+    line = line.strip()
+    
+    if len(line) == 0:
+        highest_count_list.append(calorie_counter)
+        calories = 0
+        calorie_counter = 0
+        
+    else:
+        calories = int(line)
+        calorie_counter = calories + calorie_counter
+
+    if calorie_counter > highest_count:
+        highest_count = calorie_counter
+    
+    print(calories, calorie_counter)
   
 file1.close()
+
+
+print(highest_count)
+
+#print(highest_count_list)
+
+highest_count_list.sort(reverse=True)
+
+print(sum(highest_count_list[0:3]))
+
+#print (highest_count)
+#print (highest_count_list)
